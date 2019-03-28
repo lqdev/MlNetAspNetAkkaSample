8f4b173f6c9e90d5ad530eea623a937542d96c1f
LQ <lqdev@outlook.com>
2019-03-27 22:37:12 -0400

    Removed unnecessary LoadColumn attributes

--- WebApi/Domain/Entities/IrisData.cs
+++ WebApi/Domain/Entities/IrisData.cs
@@ -1,18 +1,11 @@
-using Microsoft.ML.Data;
-
 namespace WebApi.Domain.Entities
 {
     public class IrisData
     {
-        [LoadColumn(0)]
         public float SepalLength { get; set; }
-        [LoadColumn(1)]
         public float SepalWidth { get; set; }
-        [LoadColumn(2)]
         public float PetalLength { get; set; }
-        [LoadColumn(3)]
         public float PetalWidth { get; set; }
-        [LoadColumn(4)]
         public string Label { get; set; }
     }
 }
